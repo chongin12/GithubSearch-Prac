5f140768ef2d013c896599cbc5330fdd261b6a06
정종인 <chongin12@naver.com>
2020-06-07 13:37:04 +0900

    Initial Commit

---
+++ GithubSearch-Prac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch-Prac
+//
+//  Created by 정종인 on 2020/06/07.
+//  Copyright © 2020 swmaestro10th. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
